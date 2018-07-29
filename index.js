a7bbba1a6f836e7788f6182c4fdf3539e76feb40
Peter Eck <jpeckiii@gmail.com>
2018-07-29 12:11:44 -0500

    Added socket functionality

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ const server = app.listen(3000, function() {
 });
 
 const io = socket(server);
-io.on("connection", function() {
-    console.log("Socket Connection works!");
+
+io.on("connection", function(socket) {
+    console.log("Socket Connection works!", socket.id);
+
+    //  Listens for the chat emit from the client
+    socket.on("chat", function(data) {
+        // Refers to all sockets on the server
+        io.emit("chat", data);
+    })
+
+    // Broadcast sends a message to all sockets but the one involved in an action
+    socket.on("typing", function(data) {
+        socket.broadcast.emit("typing", data);
+    });
 });
 
